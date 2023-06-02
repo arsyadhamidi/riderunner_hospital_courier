1fa8ed7f436378439cbef092da5627e42709231a
arsyadhamidi <arsyadhamidi04@gmail.com>
2023-06-02 18:54:22 +0700

    Detail task batch 1

---
+++ lib/model/delivery_model.dart
@@ -0,0 +1,34 @@
+class ModelDelivery{
+  String namaDelivery;
+  String banyakObat;
+
+  ModelDelivery(
+      this.namaDelivery,
+      this.banyakObat,
+      );
+
+}
+
+List<ModelDelivery>? listDelivery = [
+  ModelDelivery(
+      'Acarbose',
+      'X2',
+  ),
+  ModelDelivery(
+    'Acarbose',
+    'X2',
+  ),
+  ModelDelivery(
+    'Acarbose',
+    'X2',
+  ),
+  ModelDelivery(
+    'Acarbose',
+    'X2',
+  ),
+  ModelDelivery(
+    'Acarbose',
+    '',
+  ),
+
+];
--- lib/ui/modules/detail_obat/detail_obat_page.dart
+++ lib/ui/modules/detail_obat/detail_obat_page.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:riderunner_hospital_courier/model/obat_model.dart';
-import 'package:riderunner_hospital_courier/ui/modules/detail_obat/detail_obat_page.dart';
+import 'package:riderunner_hospital_courier/model/delivery_model.dart';
 import 'package:side_sheet/side_sheet.dart';
 
-class DetailCourierPage extends StatefulWidget {
-  const DetailCourierPage({Key? key}) : super(key: key);
+class DetailObatPage extends StatefulWidget {
+  const DetailObatPage({Key? key}) : super(key: key);
 
   @override
-  State<DetailCourierPage> createState() => _DetailCourierPageState();
+  State<DetailObatPage> createState() => _DetailObatPageState();
 }
 
-class _DetailCourierPageState extends State<DetailCourierPage> {
+class _DetailObatPageState extends State<DetailObatPage> {
+
+
   @override
   void initState() {
-    listObat?.toList(growable: true);
+    listDelivery?.toList(growable: true);
     super.initState();
   }
 
@@ -43,11 +44,6 @@ class _DetailCourierPageState extends State<DetailCourierPage> {
             ),
           ),
         ),
-        title: Center(
-            child: Text(
-          "Detail Task Batch 1",
-          style: TextStyle(color: Colors.black),
-        )),
         actions: [
           IconButton(
               onPressed: () {
@@ -99,7 +95,7 @@ class _DetailCourierPageState extends State<DetailCourierPage> {
                             children: [
                               Row(
                                 mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                                MainAxisAlignment.spaceBetween,
                                 children: [
                                   Row(
                                     children: [
@@ -120,7 +116,7 @@ class _DetailCourierPageState extends State<DetailCourierPage> {
                               SizedBox(height: 30),
                               Row(
                                 mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                                MainAxisAlignment.spaceBetween,
                                 children: [
                                   Row(
                                     children: [
@@ -141,7 +137,7 @@ class _DetailCourierPageState extends State<DetailCourierPage> {
                               SizedBox(height: 30),
                               Row(
                                 mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                                MainAxisAlignment.spaceBetween,
                                 children: [
                                   Row(
                                     children: [
@@ -162,7 +158,7 @@ class _DetailCourierPageState extends State<DetailCourierPage> {
                               SizedBox(height: 30),
                               Row(
                                 mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                                MainAxisAlignment.spaceBetween,
                                 children: [
                                   Row(
                                     children: [
@@ -183,7 +179,7 @@ class _DetailCourierPageState extends State<DetailCourierPage> {
                               SizedBox(height: 30),
                               Row(
                                 mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                                MainAxisAlignment.spaceBetween,
                                 children: [
                                   Row(
                                     children: [
@@ -204,7 +200,7 @@ class _DetailCourierPageState extends State<DetailCourierPage> {
                               SizedBox(height: 30),
                               Row(
                                 mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                                MainAxisAlignment.spaceBetween,
                                 children: [
                                   Row(
                                     children: [
@@ -241,214 +237,81 @@ class _DetailCourierPageState extends State<DetailCourierPage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                padding: const EdgeInsets.all(20),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 70,
-                      width: 70,
-                      decoration: BoxDecoration(
-                        color: Color.fromRGBO(217, 217, 217, 1),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    const SizedBox(width: 15),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                            width: 240,
-                            child: const Text(
-                                "Nama Doctor yang menyediakan Task ini",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16))),
-                        const SizedBox(height: 5),
-                        Text("Profesinya doctor disini",
-                            style: TextStyle(fontSize: 13, color: Colors.grey)),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Location",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18)),
-                        SizedBox(height: 7),
-                        Text("Rumah Sakit A - Farmasi",
-                            style: TextStyle(color: Colors.grey)),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Jumlah Pesakit",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18)),
-                        SizedBox(height: 7),
-                        Text("5 Pesakit", style: TextStyle(color: Colors.grey)),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Batas Waktu",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18)),
-                        SizedBox(height: 7),
-                        Text("11:30", style: TextStyle(color: Colors.grey)),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Shipping Costs",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18)),
-                        SizedBox(height: 7),
-                        Text("RM 25", style: TextStyle(color: Colors.grey)),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                height: 250,
-                width: double.infinity,
-                color: Color.fromRGBO(255, 184, 0, 1),
-                child: Stack(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Container(
-                        height: 70,
-                        width: 190,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(15),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Disini nama hospitalnya",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                              SizedBox(height: 3),
-                              Text("Lihat peta lebih besar",
-                                  style: TextStyle(
-                                      color: Colors.blue,
-                                      decoration: TextDecoration.underline)),
-                            ],
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      height: 35,
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.withOpacity(0.2),
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.arrow_back_ios_new,
-                            color: Colors.grey,
-                            size: 18,
-                          ),
-                          SizedBox(width: 7),
-                          Text(
-                            "Previous",
-                            style: TextStyle(color: Colors.grey),
-                          )
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Container(
-                      height: 35,
-                      width: 80,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Next"),
-                          SizedBox(width: 7),
-                          Icon(
-                            Icons.arrow_forward_ios,
-                            color: Colors.grey,
-                            size: 18,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
                 padding: EdgeInsets.all(20),
-                child: Text("List Obat Pesakit",
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                child: Text("This Assignment has not been Approved"),
               ),
-              Padding(
-                padding: EdgeInsets.only(left: 20, right: 20),
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: listObat?.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      elevation: 3,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      child: ListTile(
-                        onTap: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => DetailObatPage()));
-                        },
-                        title: Text(
-                          listObat?[index].namaObat ?? '',
-                          style: TextStyle(fontWeight: FontWeight.bold),
+              Container(
+                width: double.infinity,
+                child: Card(
+                  elevation: 3,
+                  child: Padding(
+                      padding: EdgeInsets.only(left: 30, top: 15, bottom: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Imam Taufiq Al - Hafidz", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        SizedBox(height: 10),
+                        Container(
+                          height: 1,
+                          width: double.infinity,
+                          color: Colors.black,
                         ),
-                        trailing: Icon(Icons.arrow_forward_ios),
+                        SizedBox(height: 10),
+                        Container(
+                          width: 300,
+                            child: Text("13, Jalan Kota Kecil, Bandar Kota Tinggi, 81900 Kota Tinggi, Johor, Malaysia")
+                        ),
+                        SizedBox(height: 5),
+                        Text("Telp : +60 1234567890"),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              Container(
+                width: double.infinity,
+                child: Card(
+                  elevation: 3,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Text("Medicine to be Delivered", style: TextStyle(color: Color.fromRGBO(0, 71, 255, 1), fontWeight: FontWeight.bold)),
                       ),
-                    );
-                  },
+                      Padding(
+                          padding: EdgeInsets.only(left: 20),
+                        child: ListView.builder(
+                            physics: NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: listDelivery?.length,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                children: [
+                                  ListTile(
+                                    leading: Image.asset('assets/images/obat.png'),
+                                    title: Text(listDelivery?[index].namaDelivery ?? ''),
+                                    trailing: Text(listDelivery?[index].banyakObat ?? '', style: TextStyle(fontWeight: FontWeight.bold),),
+                                  ),
+                                  Divider(
+                                    color: Colors.black,
+                                  ),
+                                ],
+                              );
+                            },
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.all(20),
+                        child: ListTile(
+                          leading: Image.asset("assets/images/cost.png", scale: 10,),
+                          title: Text("Cost Penghantaran", style: TextStyle(fontWeight: FontWeight.bold),),
+                          trailing: Text("RM 2", style: TextStyle(fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               )
             ],
